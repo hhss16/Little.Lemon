c24d43a104935cd2a575a478cd049077101a3c91
MyMac <MyMac@MyMac.lan>
2022-10-05 11:05:16 +0600

    Serializer

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import  get_object_or_404
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view 
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
-# Create your views here.
+  
+
+# Create your views here. 
+
+@api_view() 
+def menu_items(request): 
+    #return Response('list of books', status=status.HTTP_200_OK) 
+    items = MenuItem.objects.all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response(serialized_item.data)
+
+@api_view() 
+def single_item(request, id):
+    # item = MenuItem.objects.get(pk=id)
+    item = get_object_or_404(MenuItem,pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
