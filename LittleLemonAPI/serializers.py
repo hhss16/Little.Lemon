df7675b67cbbe9ffd20875b35e48813cf253b1d6
Hasin Hayder <hasin@leevio.com>
2022-10-09 16:03:38 +0600

    Desrialization

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -13,7 +13,7 @@ class MenuItemSerializer(serializers.ModelSerializer):
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     class Meta:
         model = MenuItem
-        fields = ['id','title','price','stock', 'price_after_tax']
+        fields = ['id','title','price','stock','price_after_tax']
     
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -3,17 +3,24 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view 
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from rest_framework import status
 
   
 
 # Create your views here. 
 
-@api_view() 
+@api_view(['GET','POST']) 
 def menu_items(request): 
     #return Response('list of books', status=status.HTTP_200_OK) 
-    items = MenuItem.objects.all()
-    serialized_item = MenuItemSerializer(items, many=True)
-    return Response(serialized_item.data)
+    if(request.method=='GET'):
+        items = MenuItem.objects.all()
+        serialized_item = MenuItemSerializer(items, many=True)
+        return Response(serialized_item.data)
+    elif request.method=='POST':
+        serialized_item = MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.validated_data,status.HTTP_201_CREATED)
 
 @api_view() 
 def single_item(request, id):
