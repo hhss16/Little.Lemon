6323063f646fcd5617bacaaed3722f572bf0086b
Hasin Hayder <hasin@leevio.com>
2022-10-11 15:53:20 +0600

    Renderers

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'LittleLemonAPI'
+    'LittleLemonAPI',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+    ]
+}
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import  get_object_or_404
 from rest_framework.response import Response 
-from rest_framework.decorators import api_view 
+from rest_framework.decorators import api_view, renderer_classes
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 from rest_framework import status
-
   
 
 # Create your views here. 
