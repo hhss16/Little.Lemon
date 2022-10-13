ca0ece154e43fde33c0574e29a612a58ab3737e6
Hasin Hayder <hasin@leevio.com>
2022-10-13 11:15:28 +0600

    Crud using Generics

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,12 +1,9 @@
-from tkinter import Menu
-from django.shortcuts import render
-from rest_framework.response import Response 
 from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
 # Create your views here.
-class MenuItemsView(generics.ListAPIView, generics.CreateAPIView):
+class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
