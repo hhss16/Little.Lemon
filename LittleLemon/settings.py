1fdbaffebf068a612045ce9bf1b0a532106c24c1
Hasin Hayder <hasin@leevio.com>
2022-10-13 19:48:34 +0600

    Token Auth

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'LittleLemonAPI',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
