86ac40b7c8970b24a1c21eac019a43db39e6dbdc
MyMac <MyMac@MyMac.local>
2022-10-14 08:36:56 +0600

    djosers

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('LittleLemonAPI.urls')),
-    re_path(r'^auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
