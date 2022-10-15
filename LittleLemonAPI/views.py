97b83babb7d15b8c7291ea8054404ff81b9edb07
MyMac <MyMac@MyMac.local>
2022-10-15 20:20:46 +0600

    Ordering

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -17,12 +17,20 @@ def menu_items(request):
         category_name = request.query_params.get('category')
         to_price = request.query_params.get('to_price')
         search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
+
+
         if category_name:
             items = items.filter(category__title=category_name)
         if to_price:
             items = items.filter(price=to_price)
         if search:
             items = items.filter(title__contains=search)
+
+        if ordering:
+            for ordering_field in ordering.split(","):
+                items = items.order_by(ordering_field)
+
         serialized_item = MenuItemSerializer(items, many=True)
         return Response(serialized_item.data)
     elif request.method=='POST':
