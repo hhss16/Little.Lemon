4212f3abf6dcb0d81ab44b6a2ca5a0bd219e7d98
Hasin Hayder <hasin@leevio.com>
2022-10-17 12:12:21 +0600

    import

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -17,11 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
