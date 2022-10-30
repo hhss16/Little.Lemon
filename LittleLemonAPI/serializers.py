8eac17829eccf9ac37e9a8ef7329fd512c8dfacf
Hasin Hayder <hasin@leevio.com>
2022-10-30 21:08:50 +0600

    Other Serializers

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -9,7 +9,7 @@ class CategorySerializer (serializers.ModelSerializer):
         model = Category
         fields = ['id','slug','title']
 
-class MenuItemSerializer(serializers.ModelSerializer):
+class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
     stock =  serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     # category = CategorySerializer()
@@ -20,7 +20,7 @@ class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id','title','price','stock', 'price_after_tax','category']
-        depth=1
+        # depth=1
     
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)
