04b24a4b9491b663585ed6a49196013e2f8d8d93
Hasin Hayder <hasin@leevio.com>
2022-11-03 13:11:35 +0600

    Progress

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,8 +1,6 @@
-from typing import Collection
 from rest_framework import serializers
 from decimal import Decimal
-from .models import MenuItem 
-from .models import Category 
+from .models import MenuItem, Category
 
 class CategorySerializer (serializers.ModelSerializer):
     class Meta:
@@ -14,9 +12,13 @@ class MenuItemSerializer(serializers.ModelSerializer):
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     category_id = serializers.IntegerField(write_only=True)
     category = CategorySerializer(read_only=True)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=2)
     class Meta:
         model = MenuItem
         fields = ['id','title','price','stock', 'price_after_tax','category','category_id']
+        extra_kwargs = {
+
+        }
     
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)
